4d208b5bba16e1aa73c29861b292c0d05976eecf
cserpakzalan2002 <114362529+cserpakzalan2002@users.noreply.github.com>
2023-04-03 20:20:55 +0200

    Update GYAK05.py
    kesz

--- GYAK/GYAK05/GYAK05.py
+++ GYAK/GYAK05/GYAK05.py
@@ -1,15 +1,14 @@
-import pandas as pd
+import numpy as np
 import seaborn as sns
 from typing import Tuple
 from scipy.stats import mode
 from sklearn.metrics import confusion_matrix
-import math
 
 
-csv_path="diabetes.csv"
+csv_path = "iris.csv"
 
 class KNNClassifier:
-
+    
     #Constructor
     def __init__(self,k:int,test_split_ratio:float):
         self.k = k
@@ -27,44 +26,44 @@ class KNNClassifier:
 
     #Load data, clean
     @staticmethod
-    def load_csv(csv_path:str) ->Tuple[pd.DataFrame,pd.Series]:
-        df = pd.read_csv(csv_path)
-        df = df.sample(frac=1, random_state=42).reset_index(drop=True)
-        x, y = df.iloc[:,:-1], df.iloc[:, -1]
-        return x, y
-
+    def load_csv(csv_path:str) ->Tuple[np.ndarray,np.ndarray]:
+        np.random.seed(42)
+        dataset = np.genfromtxt(csv_path,delimiter=',')
+        np.random.shuffle(dataset,)
+        x,y = dataset[:,:4],dataset[:,-1]
+        return x,y
+    
     #Train test split try optim
-    def train_test_split(self,features:pd.DataFrame,labels:pd.Series)-> None:
+    def train_test_split(self,features:np.ndarray,labels:np.ndarray)-> None:
         test_size = int(len(features) * self.test_split_ratio)
         train_size = len(features) - test_size
         assert len(features) == test_size + train_size, 'Size mismatch!'
-
-        self.x_train = features.iloc[:train_size,:]
-        self.y_train = labels.iloc[:train_size]
-
-        self.x_test = features.iloc[train_size:train_size+test_size,:]
-        self.y_test = labels.iloc[train_size:train_size + test_size]
-
-
-    def euclidean(self,element_of_x:pd.DataFrame) -> pd.DataFrame:
-        return math.sqrt(pd.DataFrame.sum((self.x_train - element_of_x)**2,axis=1))
     
+        self.x_train = features[:train_size,:]
+        self.y_train = labels[:train_size]
 
-    def predict(self, x_test:pd.DataFrame) ->pd.DataFrame:
-        labels_pred=[]
+        self.x_test = features[train_size:train_size+test_size,:]
+        self.y_test = labels[train_size:train_size + test_size]
+    
+    def euclidean(self,element_of_x:np.ndarray) -> np.ndarray:
+        return np.sqrt(np.sum((self.x_train - element_of_x)**2,axis=1))
+
+    def predict(self,x_test:np.ndarray) -> np.ndarray:
+        labels_pred = []
         for x_test_element in x_test:
-            distances = self.euclidean(self.x_train,x_test_element)
-            distances = pd.DataFrame(sorted(zip(distances,self.y_train)))
+            distances = self.euclidean(x_test_element)
+            distances = np.array(sorted(zip(distances,self.y_train)))
             label_pred = mode(distances[:self.k,1],keepdims=False).mode
             labels_pred.append(label_pred)
-        self.y_preds = pd.DataFrame.array(labels_pred,dtype=pd.Int32Dtype)
-        return self.y_preds
-    
+        self.y_preds = np.array(labels_pred,dtype=np.int32)
+
     def accuracy(self) -> float:
         true_positive = (self.y_test == self.y_preds).sum()
         return true_positive / len(self.y_test) * 100
     
-    def plot_confusion_matrix(self):
+    def confusion_matrix(self) -> np.ndarray:
         conf_matrix = confusion_matrix(self.y_test,self.y_preds)
         return conf_matrix
+    
+
 
