2a74d1c3556e86bac56073c00ca892be4f892790
cserpakzalan2002 <114362529+cserpakzalan2002@users.noreply.github.com>
2023-05-14 18:52:21 +0200

    Create HAZI11.py
    done

---
+++ HAZI/HAZI11/HAZI11.py
@@ -0,0 +1,106 @@
+# %%
+
+import tensorflow as tf
+import numpy as np
+
+'''
+Készíts egy metódust ami a cifar100 adatbázisból betölti a train és test adatokat. (tf.keras.datasets.cifar100.load_data())
+Majd a tanitó, és tesztelő adatokat normalizálja, és vissza is tér velük.
+
+
+Egy példa a kimenetre: train_images, train_labels, test_images, test_labels
+függvény neve: cifar100_data
+'''
+
+def cifar100_data():
+    (train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.cifar100.load_data()
+    train_images = train_images / 255.0
+    test_images = test_images / 255.0
+    return train_images, train_labels, test_images, test_labels
+
+
+
+
+# %%
+'''
+Készíts egy konvolúciós neurális hálót, ami képes felismerni a képen mi van a 100 osztály közül.
+A háló kimenete legyen 100 elemű, és a softmax aktivációs függvényt használja.
+Hálon belül tetszőleges számú réteg lehet..
+
+
+Egy példa a kimenetre: model,
+return type: keras.engine.sequential.Sequential
+függvény neve: cifar100_model
+'''
+
+
+
+def cifar100_model():
+    num_classes = 100
+
+    model = tf.keras.models.Sequential()
+    
+    model.add(tf.keras.layers.Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=(32, 32, 3)))
+    model.add(tf.keras.layers.Conv2D(32, (3, 3), padding='same', activation='relu'))
+    model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+    
+    model.add(tf.keras.layers.Conv2D(64, (3, 3), padding='same', activation='relu'))
+    model.add(tf.keras.layers.Conv2D(64, (3, 3), padding='same', activation='relu'))
+    model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+    
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(512, activation='relu'))
+    model.add(tf.keras.layers.Dropout(0.5))
+    model.add(tf.keras.layers.Dense(num_classes, activation='softmax'))
+    
+    return model
+
+
+
+# %%
+'''
+Készíts egy metódust, ami a bemeneti hálot compile-olja.
+Optimizer: Adam
+Loss: SparseCategoricalCrossentropy(from_logits=False)
+
+Egy példa a bemenetre: model
+Egy példa a kimenetre: model
+return type: keras.engine.sequential.Sequential
+függvény neve: model_compile
+'''
+def model_compile(model):
+    optimizer = tf.keras.optimizers.Adam()
+    loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False)
+
+    model.compile(optimizer=optimizer, loss=loss, metrics=['accuracy'])
+    
+    return model
+
+
+# %%
+'''
+Készíts egy metódust, ami a bemeneti hálót feltanítja.
+
+Egy példa a bemenetre: model,epochs, train_images, train_labelsz
+Egy példa a kimenetre: model
+return type: keras.engine.sequential.Sequential
+függvény neve: model_fit
+'''
+def model_fit(model, epochs, train_images, train_labels):
+    model.fit(train_images, train_labels, epochs=epochs, batch_size=128)
+    return model
+
+
+
+# %%
+'''
+Készíts egy metódust, ami a bemeneti hálót kiértékeli a teszt adatokon.
+
+Egy példa a bemenetre: model, test_images, test_labels
+Egy példa a kimenetre: test_loss, test_acc
+return type: float, float
+függvény neve: model_evaluate
+'''
+def model_evaluate(model, test_images, test_labels):
+    test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+    return test_loss, test_acc
